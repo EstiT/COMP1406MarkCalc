caac78127d67e20eb9f1334af451d0c187ce9f54
Esti Tweg <estictweg@hotmail.com>
2016-02-18 14:25:39 -0500

    Include the correct file and test functions more clearly

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
-#include "COMP1406W16A1.h"
+#include "COMP1406MarkCalc.cpp"
 
 int main(){
 
-    std::cout<<"The printed result here should equal 100   -> " << calculateYourFinalGrade(100, 100, 0, 100, 100, 100) << std::endl;
+    std::cout<<"The printed result here should equal 100  -> " << calculateYourFinalGrade(100, 100, 0, 100, 100, 100) << std::endl;
 
-    std::cout<<"The printed result here should equal true  -> " << didYouPassTheCourse(50, 50, 50, 50) << std::endl;
+    std::cout<<"The printed result here should equal true ('1')  -> " << didYouPassTheCourse(50, 50, 50, 50) << std::endl;
 
-    std::cout<<"The printed result here should equal A+    -> ";
+    std::cout<<"The printed result here should equal A+  -> ";
 
     printLetterGrade(99);
 
     float testClass [] = {100, 50};
 
-    std::cout<<"The printed result here should equal 75%   -> " << calculateClassStats(testClass, 2) << std::endl;
+    std::cout<<"The printed result here should be:" <<std::endl;
+	std::cout<<"Percentage: 100 Letter Grade: A+" <<std::endl;
+	std::cout<<"Percentage: 50 Letter Grade: D-"<<std::endl;
+	std::cout<<"75"<<std::endl;
+	std::cout<<"---------------------------------"<<std::endl;
+
+	std::cout<<calculateClassStats(testClass, 2) << std::endl;
 
     return 0;
 }
